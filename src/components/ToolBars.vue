aa60d978ca4c6f700ef6d1d89aa606595556a06d
Franky <kirymarukrak@gmail.com>
2024-03-02 12:03:05 -0500

    por la showPage

--- src/components/ToolBars.vue
+++ src/components/ToolBars.vue
@@ -1,66 +1,56 @@
 <template>
   <q-header elevated class="bg-color">
-  <q-toolbar class="q-pa-sm q-gutter-sm row justify-between">
-  <q-btn
-    dense
-    flat
-    round
-    icon="menu"
-    @click="leftDrawerOpen = !leftDrawerOpen"
-    color="black"
-  />
-  <q-btn-group flat dense class="row justify-end">
-    <q-btn
-      label="Perfil"
-      dense
-      flat
-      round
-      icon="person"
-      color="black"
-      to="user"
-    />
-    <q-btn
-      label="Logout"
-      dense
-      flat
-      round
-      icon="logout"
-      color="black"
-      to="login"
-    />
-  </q-btn-group>
-</q-toolbar>
+    <q-toolbar class="q-pa-sm q-gutter-sm row justify-end">
+      <q-btn-group flat dense class="row justify-end" >
+        <q-btn-dropdown color="bg-color" icon="add" text-color="black" >
+          <q-list>
+            <q-item clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>Profesor</q-item-label>
+              </q-item-section>
+            </q-item>
 
+            <q-item clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>Aval de Publicación</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>Aval de Tutorias</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>Aval de Bibliografia</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+        <q-btn-dropdown color="bg-color" icon="person" text-color="black" align="between">
+          <q-list>
+            <q-item
+              icon="person"
+              to="user"
+              clickable
+              v-close-popup
+              @click="onItemClick"
+            >
+              <q-item-section>
+                <q-item-label>Usuario</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item to="login" clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>Logout</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-btn-group>
+    </q-toolbar>
   </q-header>
-  <q-drawer
-    class="bg-color"
-    v-model="leftDrawerOpen"
-    show-if-above
-    bordered
-    content-class="bg-grey-1"
-  >
-    <q-list>
-      <q-item clickable v-ripple to="/">
-        <q-item-section>Home</q-item-section>
-      </q-item>
-      <q-item clickable v-ripple to="profe">
-        <q-item-section>Profesores</q-item-section>
-      </q-item>
-      <q-item clickable v-ripple to="arti">
-        <q-item-section>Revistas</q-item-section>
-      </q-item>
-      <q-item clickable v-ripple to="libro">
-        <q-item-section>Libros</q-item-section>
-      </q-item>
-    </q-list>
-  </q-drawer>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      leftDrawerOpen: false,
-    };
-  },
-};
-</script>
+<script></script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,115 @@
 <template>
+  <div class="q-pa-lg">
+    <q-table
+      title="Recursos"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :filter="search"
+      pidap
+    >
+      <template v-slot:top-right>
+        <q-input dense outlined v-model="search" placeholder="Buscar" />
+      </template>
 
-  <q-page class="row items-center justify-evenly">
-    <div>
-       <carrusel-img/>
-    </div>
-  </q-page>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.value }}
+          </q-td>
+
+          <q-td auto-width>
+            <q-btn
+              color="secondary"
+              icon="visibility"
+              size="sm"
+              to="detalles"
+
+            />
+            <q-btn
+              color="primary"
+              icon="edit"
+              size="sm"
+
+            />
+            <q-btn
+              color="negative"
+              icon="delete"
+              size="sm"
+
+            />
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+  </div>
 </template>
 
 <script setup lang="ts">
-import CarruselImg from 'src/components/CarruselImg.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const search = ref('');
+const rows = ref([]);
+const columns = [
+  {
+    name: 'nombre',
+    required: true,
+    label: 'Nombre ',
+    align: 'left',
+    filter: true,
+    sortable: true,
+  },
+  {
+    name: 'apellidos',
+    required: true,
+    label: ' Apellidos',
+
+    filter: true,
+    sortable: true,
+  },
+
+
+  {
+    name: 'titulo_recurso',
+    label: 'Titulo del Recurso',
+    field: 'titulo_recurso',
+    sortable: true,
+    filter: true,
+  },
+  {
+    name: 'fecha_publicacion',
+    label: 'Fecha de Publicacion',
+    field: 'fecha_publicacion',
+    sortable: true,
+    filter: true,
+  },
+  {
+    name: 'lugar_pub',
+    label: 'Lugar de Publicacion',
+    field: 'lugar_pub',
+    sortable: true,
+    filter: true,
+  },
+];
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:5173/api/profesores/');
+    rows.value = response.data;
+  } catch (error) {
+    console.error('Error al obtener los datos de los profesores:', error);
+  }
+});
+
+// Métodos para manejar las acciones de los botones
+//const deleteRow = (row) => {
+// Lógica para eliminar la fila
+//};
+
+//const updateRow = (row) => {
+// Lógica para actualizar la fila
+//};
+
+//const showRow = (row) => {
+// Lógica para mostrar la fila
 </script>

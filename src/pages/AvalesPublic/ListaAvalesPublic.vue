8f5e6b86899208097e3672b28ef9d6eb9e2e9089
Franky <kirymarukrak@gmail.com>
2024-06-02 04:40:43 -0400

    avanses en el show de los avales

---
+++ src/pages/AvalesPublic/ListaAvalesPublic.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="q-pa-lg">
+    <q-table
+      title="Avales de Publicación"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :filter="search"
+      pidap
+    >
+      <template v-slot:top-right>
+        <q-btn
+          icon="dehaze "
+          size="md"
+          flat
+          dense
+
+          to="detalles"
+        />
+        <q-input dense outlined v-model="search" placeholder="Buscar" />
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.value }}
+          </q-td>
+
+          <q-td auto-width>
+            <q-btn
+              icon="visibility"
+              size="sm"
+              flat
+              dense
+              @click="showRow(props.row)"
+            />
+            <q-btn
+              icon="edit"
+              size="sm"
+              flat
+              dense
+              @click="editRow(props.row)"
+            />
+            <q-btn
+              icon="delete"
+              size="sm"
+              class="q-ml-sm"
+              flat
+              dense
+              @click="deleteRow(props.row)"
+            />
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+    <q-dialog v-model="editDialogOpen">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Editar Recurso</div>
+        </q-card-section>
+
+        <q-card-section>
+          <q-input v-model="editForm.nombre" label="Nombre" />
+          <q-input v-model="editForm.apellidos" label="Apellidos" />
+          <q-input
+            v-model="editForm.titulo_recurso"
+            label="Titulo del Recurso"
+          />
+          <q-input
+            v-model="editForm.departamento"
+            label="Departamento de Trabajo"
+          />
+          <q-input v-model="editForm.lugar_pub" label="Lugar de Publicacion" />
+          <q-input v-model="editForm.tomo" label="Tomo" />
+          <q-input v-model="editForm.folio" label="Folio" />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat rounded label="Cancelar" v-close-popup />
+          <q-btn
+            flat
+            rounded
+            color="primary"
+            label="Guardar"
+            @click="saveEdit"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+const search = ref('');
+const rows = ref([]);
+const router = useRouter();
+const columns = [
+  {
+    name: 'nombre',
+    required: true,
+    label: 'Nombre ',
+    align: 'left',
+    field: 'nombre',
+    filter: true,
+    sortable: true,
+  },
+  {
+    name: 'apellidos',
+    required: true,
+    label: ' Apellidos',
+    field: 'apellidos',
+    filter: true,
+    sortable: true,
+  },
+
+  {
+    name: 'titulo_recurso',
+    label: 'Titulo del Recurso',
+    field: 'titulo_recurso',
+    sortable: true,
+    filter: true,
+  },
+  {
+    name: 'departamento',
+    label: 'Departamento de Trabajo',
+    field: 'departamento',
+    sortable: true,
+    filter: true,
+  },
+  {
+    name: 'fecha',
+    label: 'Fecha de Publicación',
+    field: 'fecha',
+    sortable: true,
+    filter: true,
+  },
+  {
+    name: 'lugar_pub',
+    label: 'Lugar de Publicación',
+    field: 'lugar_pub',
+    sortable: true,
+    filter: true,
+  },
+];
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/profesores/');
+    console.log('Datos obtenidos con éxito:', response.data.results);
+    rows.value = response.data.results;
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+  }
+});
+
+const editDialogOpen = ref(false);
+const editForm = ref({
+  nombre: '',
+  apellidos: '',
+  titulo_recurso: '',
+  departamento: '',
+  lugar_pub: '',
+  tomo: '',
+  folio: '',
+});
+const selectedRow = ref(null);
+
+const editRow = (row) => {
+  selectedRow.value = row;
+  editForm.value = {...row };
+  editDialogOpen.value = true;
+};
+
+const saveEdit = async () => {
+  try {
+    await axios.put(`/api/profesores/${selectedRow.value.id}/`, editForm.value);
+    console.log('Recurso actualizado con éxito');
+
+    editDialogOpen.value = false;
+  } catch (error) {
+    console.error('Error al actualizar el recurso:', error);
+  }
+};
+
+const showRow = (row) => {
+  console.log('Mostrando detalles del recurso:', row);
+  router.push({ name: 'show', params: { id: row.id } });
+};
+
+const deleteRow = async (row) => {
+  try {
+    await axios.delete(`/api/profesores/${row.id}/`);
+    console.log('Recurso eliminado con éxito');
+
+    rows.value = rows.value.filter((item) => item.id!== row.id);
+  } catch (error) {
+    console.error('Error al eliminar el recurso:', error);
+  }
+};
+</script>
